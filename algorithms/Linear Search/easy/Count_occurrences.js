b3db38b7ebd50b3c1e09e42803c66fcab2d68783
meSurajKumar <mesuraj751@gmail.com>
2025-11-28 23:58:46 +0530

    added some exaple for the liner search

--- algorithms/Linear Search/easy/Count_occurrences.js
+++ algorithms/Linear Search/easy/Count_occurrences.js
@@ -2,7 +2,7 @@
 // Given an array arr[] and value x, count how many times x appears in the array. Return the count (can be zero).
 // Purpose: Practice full traversal + counting.
 
-const arrayData = [1,2,3,4,42,5,6,7,19,42,42,44,42,42];
+// const arrayData = [1,2,3,4,42,5,6,7,19,42,42,44,42,42];
 // let count = [];
 
 // function countExistance(arrData , target){
@@ -18,12 +18,38 @@ const arrayData = [1,2,3,4,42,5,6,7,19,42,42,44,42,42];
 
 // second way;
 
-let count=0;
-function countExistance(arrData , target){
-    for(let i =0; i<arrData.length;i++){
-        if(arrData[i] === target) count++;  
-    }
-}
-countExistance(arrayData, 19)
+// let count=0;
+// function countExistance(arrData , target){
+//     for(let i =0; i<arrData.length;i++){
+//         if(arrData[i] === target) count++;  
+//     }
+// }
+// countExistance(arrayData, 19)
 
-console.log('occurrence > ', count)
+// console.log('occurrence > ', count)
+
+
+
+// for of loop
+// reduce
+// frequency map for {For counting the multiple element occurance}
+// filter
+
+// for of loop :- To count the occurance of element.
+
+const arrayData = [12,11,12,23,24,14,56,78,79,66,78];
+// let count =0
+// const occuranceFinder = (arrData , target)=>{
+//     for(item of arrayData){
+//         if(item === target) count++ 
+//     }
+// }
+
+// occuranceFinder(arrayData , 78);
+// console.log(count)
+
+// with filter.
+// const count = arrayData.filter((x)=> x==78).length
+// console.log(count)
+// but with the filter this is slightly slower because it's create new array.
+
